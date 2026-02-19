247cfa911dae9203aa602063ff70d9bf04b0fe3a
Jaway <jaway.khristal88@gmail.com>
2026-02-19 12:52:00 +0800

    WSAT-M4: JavaScript interactivity added

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ console.log("JavaScript connected successfully!");
 const themeToggle = document.getElementById("themeToggle");
 
 themeToggle.addEventListener("click", function () {
+
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeToggle.textContent = "Toggle Light Mode";
+    } else {
+        themeToggle.textContent = "Toggle Dark Mode";
+    }
 });
 
 const toggleSkills = document.getElementById("toggleSkills");
